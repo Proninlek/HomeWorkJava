41cb75759c0365a11f4985ba8f191872be70a700
DFIT Vasia <pronin_lek@mail.ru>
2023-04-27 21:32:33 +0300

    Задание про планеты готово

--- HomeWork6.java
+++ HomeWork6.java
@@ -1,9 +1,22 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import Funcstream.Collectors;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class HomeWork6 {
     public static void main(String[] args) {
         List<String> list = Arrays.asList(
         "Марс", "Юпитер", "Сатурн", "Меркурий", "Нептун",
         "Венера", "Нептун", "Сатурн", "Юпитер", "Земля",
-        "Венера", "Сатурн", "Уран", "Земля", "Марс");
+        "Венера", "Сатурн", "Уран", "Меркурий", "Земля", "Марс");
 
         Map<String, Long> frequency = 
             list.stream().collect(Collectors.groupingBy(
