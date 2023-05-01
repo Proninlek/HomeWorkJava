b412669a6fe5281507f1241c545db275b91c2b24
DFIT Vasia <pronin_lek@mail.ru>
2023-05-01 19:52:46 +0300

    дз 3 задание готово

--- HomeWork7.java
+++ HomeWork7.java
@@ -1,28 +1,20 @@
-import java.util.*;
-import java.util.Random;
+import java.util.Collections;
+import java.util.LinkedList;
 
 public class HomeWork7 {
     public static void main(String[] args) {
-        LinkedList<Integer> list = getList(5, 1, 9);
-        System.out.println(list);
-        System.out.println(getReverseList(list));
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("77");
+        list.add("25");
+        list.add("69");
+        list.add("3");
+        list.add("нога");
+        LinkedList<String> reversedList = reverseLinkedList(list);
+        System.out.println(reversedList);
     }
 
-    public static LinkedList<Integer> getList(int size, int min, int max) {
-        LinkedList<Integer> list = new LinkedList<>();
-        Random r = new Random();
-        for (int i = 0; i < size; i++) {
-            list.add(r.nextInt(min, max + 1));
-            }
+    public static LinkedList reverseLinkedList(LinkedList list) {
+        Collections.reverse(list);
         return list;
-    }
-
-    public static List<Integer> getReverseList(LinkedList<Integer> list) {
-        LinkedList<Integer> resultList = new LinkedList<>();
-        ListIterator<Integer> iterator = list.listIterator(list.size());
-        while (iterator.hasPrevious()){
-            resultList.add(iterator.previous());
-        }
-        return resultList;
     }
 }
